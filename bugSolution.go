cce400a3f2c4d5dcccc90eea6972893c5560c202
BugHunter-X <askzenai@gmail.com>
2025-01-26 16:52:05 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,8 @@
+```go
+func main() {
+    var x int
+    fmt.Println("Initial value of x:", x) // Explicitly print the initial value
+    x = 10
+    fmt.Println("Value of x after assignment:", x)
+}
+```
